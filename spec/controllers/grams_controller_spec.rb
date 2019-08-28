f911f2282492a86ec0522ac2ca9c928af3f8df3c
herangakr <ma.muthukuda@gmail.com>
2019-08-28 19:36:09 +0000

    Add FactoryBot

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -8,17 +8,15 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+
   describe "grams#new action" do
     it "should require users to be logged in" do
       get :new
       expect(response).to redirect_to new_user_session_path
     end
+
     it "should successfully show the new form" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -26,18 +24,16 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+
   describe "grams#create action" do
+
     it "should require users to be logged in" do
-      post :create, params: { grams: { message: "Hello"} }
+      post :create, params: { gram: { message: "Hello" } }
       expect(response).to redirect_to new_user_session_path
     end
 
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { gram: { message: 'Hello!' } }
@@ -45,20 +41,18 @@ RSpec.describe GramsController, type: :controller do
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
-      expect(gram.user). to eq(user)
+      expect(gram.user).to eq(user)
     end
 
-    it "should properly deal with vallidation errors" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+    it "should properly deal with validation errors" do
+      user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram: {message: '' } }
+      gram_count = Gram.count
+      post :create, params: { gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq Gram.count
+      expect(gram_count).to eq Gram.count
     end
+
   end
 end

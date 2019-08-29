574d8ed3980a97b24201a5b38423ac79ad13d889
herangakr <ma.muthukuda@gmail.com>
2019-08-29 17:54:14 +0000

    Add Ability to Destroy Grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -34,10 +34,17 @@ class GramsController < ApplicationController
 
     if @gram.valid?
       redirect_to root_path
-   else
+    else
       return render :edit, status: :unprocessable_entity
-   end
-end
+    end
+  end
+
+  def destroy
+   @gram = Gram.find_by_id(params[:id])
+   return render_not_found if @gram.blank?
+   @gram.destroy
+   redirect_to root_path
+  end
 
   private
 
